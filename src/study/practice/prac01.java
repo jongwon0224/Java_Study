0b3322ee03fb4320c4d7c329ff59678bed0c0d38
jongwon0224 <justine25@naver.com>
2024-10-18 16:59:36 +0900

    연산자 조건문 연습

---
+++ src/study/practice/prac01.java
@@ -0,0 +1,130 @@
+package study.practice;
+
+import java.util.Scanner;
+
+public class prac01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//1번
+//		1~24 / 12이하 오전, 12초과 오후, 1~6시 오전, 7~12늦은 오전, 13~18이른 오후, 19~24늦은 오후
+		// 1~24 중 하나의 정수를 입력받습니다. 범위를 넘어선 값을 입력할 수도 있음.
+		// 입력받은 숫자를 보고 아래 기준에 맞게 출력하세요.
+		// 1) 범위를 벗어나면, "잘못입력했습니다." 라고 출력
+		// 2) 시간<=12 오전입니다. 시간>12 오후입니다.
+		// 3) 1~6시 이른 오전입니다. 7~12 늦은 오전입니다.
+		// 4) 13~18시 이른 오후입니다. 19~24 늦은 오후입니다.
+
+		//입력에 따른 출력 예시
+		/*
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("정수를 입력하세요 (1 ~ 24) : ");
+		
+		int num = scanner.nextInt();
+		
+		if(num >= 1 && num <= 24) {
+			if(num <= 12) {
+				System.out.println("오전입니다.");
+				if(num <= 6) {
+					System.out.println("이른 오전입니다.");
+				} else {
+					System.out.println("늦은 오전입니다.");
+				}
+			} else {
+				System.out.println("오후입니다.");
+			} if(num <= 18) {
+				System.out.println("이른 오후입니다.");
+			} else {
+				System.out.println("늦은 오후입니다.");
+			}			
+		} else {
+			System.out.println("잘못입력했습니다.");
+		}		
+		*/
+		
+		
+		//2번 아래 코드를 if문으로 변경
+
+		int diceNum = 4; //주사위 수 1~6
+
+		// if문 변환
+		
+//		if (diceNum == 1) {
+//			System.out.println("1");
+//		} else if(diceNum == 2) {
+//			System.out.println("2");
+//		} else if(diceNum == 3) {
+//			System.out.println("3");
+//			System.out.println("4");
+//			System.out.println("5");
+//		} else if(diceNum == 4) {
+//			System.out.println("4");
+//			System.out.println("5");
+//		} else if(diceNum == 5) {
+//			System.out.println("5");
+//		} else {
+//			System.out.println("6");
+//		}
+		
+		
+		//3번 아래 코드를 swtich 문으로 변경
+		/*
+		String localNum = "031";
+		// switch 문으로 변경
+		
+		switch(localNum) {
+		case "031":
+			System.out.println("경기도입니다.");
+			break;
+			
+		case "02":
+			System.out.println("서울입니다.");
+			break;
+		
+		case "041":
+			System.out.println("충남입니다.");
+			break;
+			
+		case "051":
+			System.out.println("부산입니다.");
+			break;
+			
+		default:
+			System.out.println("다시 확인해주세요");
+		}
+		*/
+		
+		
+		//4번 아래 코드를 switch 문으로 변경
+		// *** switch 문은 범위를 지정할 수 없습니다.
+		// 결과 값만 동일하게 나오도록 만들어보세요.
+		// 단, 점수는 100점까지로 간주합니다.
+		// 점수는 0~100 점 중에 score로 주어진다.
+
+				
+		int score = 99;
+		switch(score / 10) {
+			
+		case 7:
+			System.out.println("학점 C");
+			break;
+			
+		case 8:
+			System.out.println("학점 B");
+			break;
+			
+		case 9, 10:
+			System.out.println("학점 A");
+			break;
+
+		default :
+			System.out.println("학점 F");
+				
+		}
+		
+		
+		
+	}
+
+}
---
+++ src/study/practice/quiz06.java
@@ -0,0 +1,72 @@
+package study.practice;
+
+import java.util.Scanner;
+
+public class quiz06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 1번. 삼각형의 밑변과 높이를 입력받아서 넓이를 출력하세요. (소수점까지 표현)
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("밑변 입력 : ");
+		int under = scanner.nextInt();
+		System.out.println("높이 입력 : ");
+		int height = scanner.nextInt();
+		System.out.printf("삼각형의 넓이는 %.3f", ((double)under*height/2));
+//		System.out.println("삼각형의 넓이는 " + (double)under*height/2 + "입니다." );
+		
+		// 2번. 사다리꼴의 윗변과 밑변과 높이를 입력받아서 넓이를 출력하세요. (소수점까지 표현)
+		int upper = scanner.nextInt();
+		System.out.println("윗변 입력 : ");
+		int under2 = scanner.nextInt();
+		System.out.println("밑변 입력 : ");
+		int height2 = scanner.nextInt();
+		System.out.println("높이 입력 : ");
+		System.out.printf("사다리꼴의 넓이는 = %.2f \n",((double)upper+under2)*height2/2);
+	
+	
+		// 3번. 우리반은 24명입니다. 과자가 100개 있습니다.
+		// 모두에게 공정하게 동일한 숫자의 과자를 나누어주면
+		// 한 명당 몇 개의 과자를 받게되는지와 나누어주고 나서 몇개의 과자가 남는지 출력하세요.	
+	
+//		int classNum = 24;
+//		int snack = 100;
+//		int perPerson = snack / classNum;
+//		int left = snack % classNum;
+//		System.out.println("한 명당 " + perPerson + "개를 받고 나머지는 " + left + "입니다.");
+		
+	
+		// 4번. 세자리의 정수를 입력 받아서 백의자리, 십의자리, 일의자리를 각각 출력하세요.
+		// 158
+		// 백의자리 : 1
+		// 십의자리 : 5
+		// 일의자리 : 8
+
+//		System.out.println("3자리 정수 입력 : ");
+//		int num = scanner.nextInt();
+//		int count = num / 100;
+//		System.out.println("백의자리 : " + count);
+//		int count2 = num % 100 / 10;
+//		System.out.println("십의자리 : " + count2);
+//		int count3 = num % 10;
+//		System.out.println("일의자리 : " + count3);
+		
+		
+		// 5번 x와 y 변수 안에 들어있는 서로의 값을 바꾸기
+//		int x = 10;
+//		int y = 20;
+//		System.out.println("x="+x+ " y="+y);
+//		//x와 y를 바꾼 작업
+//		
+//		int z = x;
+//		x = y;
+//		y = z;
+//		
+//		System.out.println("x="+x+ " y="+y);
+		
+		
+	
+	}
+
+}
